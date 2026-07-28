a3b074e29d0b9e41c1559c303127c72ed2f2a003
sightopsgitai-star <sightopsgitai@gmail.com>
2026-07-28 19:50:52 +0530

    Fix SyntaxError: Unexpected token ':' in memberController.js for backend Node.js execution on Render

--- server/src/controllers/memberController.js
+++ server/src/controllers/memberController.js
@@ -57,7 +57,7 @@ export const createMember = async (req, res, next) => {
     });
 
     res.status(201).json({ success: true, data: member });
-  } catch (error: any) {
+  } catch (error) {
     if (error.code === 11000) {
       return res.status(400).json({
         success: false,
